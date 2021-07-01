f4117e2ffe3e24641dcb080c38cf73a9b3175cde
Hariprasad-KK <56286476+Hariprasad-KK@users.noreply.github.com>
2021-07-01 22:46:15 +0530

    Add files via upload

---
+++ Ajax.js
@@ -0,0 +1,194 @@
+let BooksArray = [];
+let cseBOOKS = [],
+  eceBOOKS = [],
+  mechBOOKS = [];
+
+let booksitem = document.getElementById("gridbook");
+
+let cat = document.getElementById("CAT");
+
+let cat2 = document.getElementById("CAT2");
+
+function hide() {
+  cat2.style.display = "none";
+  cat.style.display = "flex";
+}
+
+function getitems() {
+  let xhr = new XMLHttpRequest();
+
+  BooksArray = [];
+  (cseBOOKS = []), (eceBOOKS = []), (mechBOOKS = []);
+  xhr.open("GET", "Books.xml", true);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let resXML = this.responseXML;
+
+      let catgory = resXML.getElementsByTagName("CATEGORY");
+      for (let i = 0; i < catgory.length; i++) {
+        let book = catgory[i].getElementsByTagName("BOOK");
+
+        for (let j = 0; j < book.length; j++) {
+          if (catgory[i].getAttribute("dep") == "cse") {
+            let temp = {
+              name: book[j].getElementsByTagName("NAME")[0].childNodes[0]
+                .nodeValue,
+              PUBSHISER: book[j].getElementsByTagName("PUBSHISER")[0]
+                .childNodes[0].nodeValue,
+              YEAR: book[j].getElementsByTagName("YEAR")[0].childNodes[0]
+                .nodeValue,
+              DIS: book[j].getElementsByTagName("DIS")[0].childNodes[0]
+                .nodeValue,
+            };
+
+            cseBOOKS.push(temp);
+          }
+
+          if (catgory[i].getAttribute("dep") == "ece") {
+            let temp = {
+              name: book[j].getElementsByTagName("NAME")[0].childNodes[0]
+                .nodeValue,
+              PUBSHISER: book[j].getElementsByTagName("PUBSHISER")[0]
+                .childNodes[0].nodeValue,
+              YEAR: book[j].getElementsByTagName("YEAR")[0].childNodes[0]
+                .nodeValue,
+              DIS: book[j].getElementsByTagName("DIS")[0].childNodes[0]
+                .nodeValue,
+            };
+
+            eceBOOKS.push(temp);
+          }
+
+          if (catgory[i].getAttribute("dep") == "mech") {
+            let temp = {
+              name: book[j].getElementsByTagName("NAME")[0].childNodes[0]
+                .nodeValue,
+              PUBSHISER: book[j].getElementsByTagName("PUBSHISER")[0]
+                .childNodes[0].nodeValue,
+              YEAR: book[j].getElementsByTagName("YEAR")[0].childNodes[0]
+                .nodeValue,
+              DIS: book[j].getElementsByTagName("DIS")[0].childNodes[0]
+                .nodeValue,
+            };
+            mechBOOKS.push(temp);
+          }
+        }
+      }
+    }
+
+    BooksArray = BooksArray.concat(cseBOOKS, eceBOOKS, mechBOOKS);
+  };
+
+  xhr.send();
+}
+
+getitems();
+
+function allbooks() {
+  booksitem.innerHTML = "";
+  for (i in BooksArray) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = BooksArray[i].name;
+    author.innerHTML = BooksArray[i].PUBSHISER;
+    year.innerHTML = BooksArray[i].YEAR;
+    dis.innerHTML = BooksArray[i].DIS;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitem.appendChild(div);
+  }
+}
+
+function csebooks() {
+  booksitem.innerHTML = "";
+  for (i in cseBOOKS) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = cseBOOKS[i].name;
+    author.innerHTML = cseBOOKS[i].PUBSHISER;
+    year.innerHTML = cseBOOKS[i].YEAR;
+    dis.innerHTML = cseBOOKS[i].DIS;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitem.appendChild(div);
+  }
+}
+
+function allece() {
+  booksitem.innerHTML = "";
+  for (i in eceBOOKS) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = eceBOOKS[i].name;
+    author.innerHTML = eceBOOKS[i].PUBSHISER;
+    year.innerHTML = eceBOOKS[i].YEAR;
+    dis.innerHTML = eceBOOKS[i].DIS;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitem.appendChild(div);
+  }
+}
+
+function allmech() {
+  booksitem.innerHTML = "";
+  for (i in mechBOOKS) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = mechBOOKS[i].name;
+    author.innerHTML = mechBOOKS[i].PUBSHISER;
+    year.innerHTML = mechBOOKS[i].YEAR;
+    dis.innerHTML = mechBOOKS[i].DIS;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitem.appendChild(div);
+  }
+}
---
+++ JSONAJAX.js
@@ -0,0 +1,181 @@
+let BooksArrayjson = [];
+let cseBOOKSjson = [],
+  eceBOOKSjson = [],
+  mechBOOKSjson = [];
+
+let booksitems = document.getElementById("gridbook");
+let cats = document.getElementById("CAT");
+let catjson = document.getElementById("CAT2");
+
+function hideXML() {
+  cats.style.display = "none";
+  catjson.style.display = "flex";
+}
+
+function getitemsJSON() {
+  let xhr = new XMLHttpRequest();
+
+  BooksArrayjson = [];
+  (cseBOOKSjson = []), (eceBOOKSjson = []), (mechBOOKSjson = []);
+  xhr.open("GET", "Books.json", true);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var resp = JSON.parse(this.responseText);
+
+      for (i in resp.books) {
+        var dep = resp.books[i].department;
+        var books = resp.books[i].book;
+        for (j in books) {
+          if (dep == "cse") {
+            let temp = {
+              name: books[j].name,
+              PUBSHISER: books[j].PUBSHISER,
+              YEAR: books[j].YEAR,
+              dis: books[j].DISC,
+            };
+            cseBOOKSjson.push(temp);
+          }
+
+          if (dep == "ece") {
+            let temp = {
+              name: books[j].name,
+              PUBSHISER: books[j].PUBSHISER,
+              YEAR: books[j].YEAR,
+              dis: books[j].DISC,
+            };
+            eceBOOKSjson.push(temp);
+          }
+
+          if (dep == "mech") {
+            let temp = {
+              name: books[j].name,
+              PUBSHISER: books[j].PUBSHISER,
+              YEAR: books[j].YEAR,
+              dis: books[j].DISC,
+            };
+            mechBOOKSjson.push(temp);
+          }
+        }
+      }
+
+      BooksArrayjson = BooksArrayjson.concat(
+        cseBOOKSjson,
+        eceBOOKSjson,
+        mechBOOKSjson
+      );
+    }
+  };
+
+  xhr.send();
+}
+
+getitemsJSON();
+
+function allbooksJSON() {
+  booksitems.innerHTML = "";
+  for (i in BooksArrayjson) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = BooksArrayjson[i].name;
+    author.innerHTML = BooksArrayjson[i].PUBSHISER;
+    year.innerHTML = BooksArrayjson[i].YEAR;
+    dis.innerHTML = BooksArrayjson[i].dis;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitems.appendChild(div);
+  }
+}
+
+function csebooksJSON() {
+  booksitems.innerHTML = "";
+  for (i in cseBOOKSjson) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = cseBOOKSjson[i].name;
+    author.innerHTML = cseBOOKSjson[i].PUBSHISER;
+    year.innerHTML = cseBOOKSjson[i].YEAR;
+    dis.innerHTML = cseBOOKSjson[i].dis;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitems.appendChild(div);
+  }
+}
+
+function alleceJSON() {
+  booksitems.innerHTML = "";
+  for (i in eceBOOKSjson) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = eceBOOKSjson[i].name;
+    author.innerHTML = eceBOOKSjson[i].PUBSHISER;
+    year.innerHTML = eceBOOKSjson[i].YEAR;
+    dis.innerHTML = eceBOOKSjson[i].dis;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitems.appendChild(div);
+  }
+}
+
+function allmechJSON() {
+  booksitems.innerHTML = "";
+  for (i in mechBOOKSjson) {
+    let div = document.createElement("div");
+    let heading = document.createElement("h1");
+    let author = document.createElement("h3");
+    let year = document.createElement("h3");
+    let dis = document.createElement("p");
+    heading.innerHTML = mechBOOKSjson[i].name;
+    author.innerHTML = mechBOOKSjson[i].PUBSHISER;
+    year.innerHTML = mechBOOKSjson[i].YEAR;
+    dis.innerHTML = mechBOOKSjson[i].dis;
+    div.style.color = "black";
+    div.style.textAlign = "center";
+    div.style.border = "2px black";
+    div.style.borderRadius = "20px";
+    div.style.padding = "10px";
+    div.style.boxShadow = "15px 15px 40px rgb(3, 3, 10)";
+    div.appendChild(heading);
+    div.appendChild(author);
+    div.appendChild(year);
+    div.appendChild(dis);
+
+    booksitems.appendChild(div);
+  }
+}
